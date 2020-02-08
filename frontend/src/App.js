78102ebbc0afe62e9af02f04b4b58065371f14e8
Orchyl Salmasan <salmasan.orchyl@gmail.com>
2020-02-08 21:16:12 +0800

    Formatted basic ReactJS requirements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Button, Container, Row, Col } from 'reactstrap';
+import ListNotes from './components/ListNotes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var notes_temp = [
+    {
+        "id": 1,
+        "title": "Django note",
+        "content": "asdasfdasdasd",
+        "created_at": "2020-02-08T10:13:17.594514Z",
+        "updated_at": "2020-02-08T10:13:17.594514Z"
+    },
+    {
+        "id": 2,
+        "title": "Python notes",
+        "content": "daoijoaushdoasd",
+        "created_at": "2020-02-08T10:13:24.960170Z",
+        "updated_at": "2020-02-08T10:13:24.960170Z"
+    },
+    {
+        "id": 3,
+        "title": "PHP",
+        "content": "Dasdasdasosdf",
+        "created_at": "2020-02-08T10:13:35.787983Z",
+        "updated_at": "2020-02-08T10:13:35.787983Z"
+    },
+]
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: notes_temp,
+      current_note_id: 0,
+      is_creating: true
+    }
+
+    this.handleItemClick = this.handleItemClick.bind(this);
+    this.handleAddNote = this.handleAddNote.bind(this);
+  }
+  handleItemClick(id) {
+      console.log("Id: ", id);
+      this.setState((prevState) => {
+          return {is_creating: false, current_note_id: id }
+      })
+  }
+  handleAddNote() {
+    this.setState((prevState) => {
+        return {is_creating: true}
+    })
+  }
+  render() {
+    return (
+        <React.Fragment>
+          <Container>
+            <Row>
+              <Col xs="10">
+                <h2>Realtime Notes</h2>
+              </Col>
+              <Col xs="2">
+                <Button color="primary" onClick={this.handleAddNote}>Create a new Note</Button>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs="4">
+                  <ListNotes notes={this.state.notes} handleItemClick={(id) => this.handleItemClick(id)} />
+              </Col>
+              <Col xs="8">
+                <p>Content for editing and deleting</p>
+                  {
+                     this.state.is_creating ?
+                         "Creating now..":
+                         `Editing note with id: ${this.state.current_note_id}`
+                  }
+              </Col>
+            </Row>
+          </Container>
+        </React.Fragment>
+    );
+  }
 }
 
 export default App;
